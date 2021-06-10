33cc75290cadf3995d29fc6f8516756f58954270
Cat <ccallcottstevens@gmail.com>
2021-06-10 16:40:03 +1000

    started todo component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import './App.css';
+import Todo from './Todo';
 
 function App() {
   return (
     <div>
       <h1 className="text-8xl text-white flex justify-center p-10 ">StreamLiveing</h1>
+    <Todo />
+    <Todo />
+    <Todo />
+    <Todo />
     </div>
   );
 }
---
+++ src/Todo.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Todo () {
+    return (
+        <div>
+            <input 
+            type='checkbox' />
+            <p> Placeholder text</p>
+        </div>
+    )
+}
+
+
+export default Todo
