0e8b1e9caddf7220b26506681e70ab45829b709c
Cat <ccallcottstevens@gmail.com>
2021-06-12 13:50:12 +1000

    localStorage initialised for todo list

--- src/App.js
+++ src/App.js
@@ -1,17 +1,15 @@
 import './App.css';
 import Todo from './Todo';
-import TodoInput from './TodoInput'
 
 function App() {
   return (
     <div >
       <h1 className="text-8xl text-white flex justify-center p-10 ">StreamLiveing</h1>
-      <div className='p-8 box-border w-2/5 border-4 border-white rounded-medium shadow-md bg-blue-900'>
+      <div className='p-8 box-border w-2/5 border-8 border-white rounded-2xl shadow-2xl bg-blue-900'>
         <Todo />
         <Todo />
         <Todo />
         <Todo />
-        <TodoInput />
       </div>
       </div>
   );
--- src/Todo.jsx
+++ src/Todo.jsx
@@ -1,13 +1,40 @@
-import React from 'react';
-import TodoList from './TodoList'
-import TodoInput from './TodoInput'
+import React, { useState, useEffect} from 'react';
 
-function Todo () {
-    return (
-        <div className='p-4 text-2xl text-white border-b-2'>
-            <TodoList />
-        </div>
-    )
+const Todo = () => {
+    
+        const [ value, setValue ] = useState( 
+        localStorage.getItem('myValueInLocalStorage') || '')
+
+        useEffect(() => {
+            localStorage.setItem('myValueInLocalStorage', value)
+        }, [value])
+
+        const onChange = event => setValue(event.target.value)
+        
+        return (
+            <div className='p-4 text-2xl text-white border-b-2'>
+            <form>
+            <input 
+                name='done'
+                type='checkbox' 
+                value={value}
+                // onChange={handleInput}
+                />
+                
+                <span> Placeholder text </span>
+                <input 
+                type='text'
+                name='newItem'
+                value={value}
+                onChange={onChange}
+                />
+                <input type="submit" 
+                value="Submit"    
+                />
+            </form>
+                
+            </div>
+        ) 
 }
 
 export default Todo
